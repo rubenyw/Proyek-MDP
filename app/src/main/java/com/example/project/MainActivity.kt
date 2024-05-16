ffd400ad097285ed3c3b7d034e75a42b3e03b19a
rubenyw <rubenyasonwinarta@gmail.com>
2024-05-16 08:59:13 +0700

    update file

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -6,9 +6,9 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Thread.sleep(3000)
-        installSplashScreen()
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        Thread.sleep(3000);
+        installSplashScreen();
+        setContentView(R.layout.activity_main);
     }
 }
