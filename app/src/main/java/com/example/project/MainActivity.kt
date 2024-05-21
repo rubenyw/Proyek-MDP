5ff97955aea53212449c581d87341e891b286cf2
rubenyw <104652395+rubenyw@users.noreply.github.com>
2024-05-21 13:08:31 +0700

    bottom navigatoin

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -3,10 +3,38 @@ package com.example.project
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.transaction
+import androidx.navigation.findNavController
+import com.example.project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root);
+
+        binding.bottomNavigation.setOnItemSelectedListener {item ->
+            when(item.itemId){
+                R.id.home_botnav -> {
+                    findNavController(binding.fragmentContainerView.id).navigate(R.id.action_global_homeFragment);
+                    true
+                }
+                R.id.give_botnav -> {
+                    findNavController(binding.fragmentContainerView.id).navigate(R.id.action_global_giveFragment);
+                    true
+                }
+                R.id.broad_botnav -> {
+                    findNavController(binding.fragmentContainerView.id).navigate(R.id.action_global_broadcastFragment);
+                    true
+                }
+                R.id.prof_botnav -> {
+                    findNavController(binding.fragmentContainerView.id).navigate(R.id.action_global_profileFragment);
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
