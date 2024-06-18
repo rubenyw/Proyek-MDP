575b75b784324287b28693b32c74cca196bb99a4
Shawn <107561331+ShawndeSheep@users.noreply.github.com>
2024-06-18 12:26:12 +0700

    Home edit 1
    home fragment progress 1

--- app/src/main/java/com/example/project/HomeFragment.kt
+++ app/src/main/java/com/example/project/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
 class HomeFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +19,19 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    lateinit var btnEvent: Button
+    lateinit var btnCommunity: Button
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnEvent = view.findViewById(R.id.btnEvent)
+        btnEvent.setOnClickListener {
+            findNavController().navigate(R.id.action_global_eventFragment)
+        }
+        btnCommunity = view.findViewById(R.id.btnCommunity)
+        btnCommunity.setOnClickListener {
+            findNavController().navigate(R.id.action_global_communityFragment)
+        }
     }
 }
