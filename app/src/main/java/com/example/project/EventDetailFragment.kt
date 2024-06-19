e73600f8aa0b3deec50eed6116abd178f2723109
nicholasWijaya100 <nich.wijaya.100@gmail.com>
2024-06-19 23:21:39 +0700

    design for event details finished

--- app/src/main/java/com/example/project/EventDetailFragment.kt
+++ app/src/main/java/com/example/project/EventDetailFragment.kt
@@ -5,11 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.text.NumberFormat
+import java.util.Locale
 
 class EventDetailFragment : Fragment() {
 
+    lateinit var buttonBack: FloatingActionButton
+    lateinit var imageViewEventImage: ImageView
+    lateinit var tvTitle: TextView
+    lateinit var tvLocation: TextView
+    lateinit var tvSchedule: TextView
+    lateinit var tvDescription: TextView
+    lateinit var tvDonation: TextView
+    private lateinit var eventId: String
+    private lateinit var eventName: String
+    private lateinit var eventLocation: String
+    private lateinit var eventDate: String
+    private lateinit var eventDescription: String
+    private lateinit var eventImageUrl: String
+    private lateinit var eventDonation: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        arguments?.let {
+            eventId = it.getString("eventId") ?: ""
+            eventName = it.getString("eventName") ?: ""
+            eventLocation = it.getString("eventLocation") ?: ""
+            eventDate = it.getString("eventDate") ?: ""
+            eventDescription = it.getString("eventDescription") ?: ""
+            eventImageUrl = it.getString("eventImageUrl") ?: ""
+            eventDonation = it.getInt("eventDonation").toString() ?: ""
+        }
     }
 
     override fun onCreateView(
@@ -17,6 +49,36 @@ class EventDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event_detail, container, false)
+        val v = inflater.inflate(R.layout.fragment_event_detail, container, false)
+
+        buttonBack = v.findViewById(R.id.floatingActionButtonBackEventDetail)
+        imageViewEventImage = v.findViewById(R.id.imageViewEventDetailPage)
+        tvTitle = v.findViewById(R.id.tvTitleEventDetailPage)
+        tvDescription = v.findViewById(R.id.tvDescriptionEventDetailPage)
+        tvDonation = v.findViewById(R.id.tvDonationEventDetail)
+        tvLocation = v.findViewById(R.id.tvLocationEventDetail)
+        tvSchedule = v.findViewById(R.id.tvDateTimeEventDetailPage)
+
+        tvTitle.setText(eventName)
+        tvDescription.setText(eventDescription)
+        tvSchedule.setText(eventDate)
+        tvLocation.setText(eventLocation)
+        tvDonation.text = formatRupiah(eventDonation.toInt())
+
+        Glide.with(this)
+            .load(eventImageUrl)
+            .into(imageViewEventImage)
+
+        buttonBack.setOnClickListener {
+            findNavController().navigate(R.id.action_global_upcomingEventsFragment)
+        }
+
+        return v
+    }
+
+    private fun formatRupiah(amount: Int): String {
+        val localeID = Locale("in", "ID")
+        val numberFormat = NumberFormat.getCurrencyInstance(localeID)
+        return numberFormat.format(amount)
     }
 }
